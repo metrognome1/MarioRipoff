9707e05cac040182ed23a9a646b599fcf8e4c3d5
metrognome1 <onceadayeveryday@gmail.com>
2016-11-11 23:41:50 -0800

    Made scripts for directional shooting.
    Created a script that allows the player to shoot either in the direction
    he is facing or up and down.

---
+++ MarioRipoff/Assets/victorscripts/ShootScripts/defaultGun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class defaultGun : MonoBehaviour, IShootable {
+    public float fireRate = 0;
+    public GameObject projectile;
+    public float projectileSpeed;
+
+    private float timeToFire = 0;
+    private Transform firePoint;
+
+    void Awake() {
+        firePoint = transform.FindChild("firePoint");
+        if (firePoint == null) {
+            Debug.LogError("No firePoint location");
+        }
+    }
+
+    public void Shoot(Vector2 direction) {
+        if (fireRate == 0) {
+            Debug.Log("Entered shoot");
+            makeProjectile(direction);
+
+        }
+        else {
+            if (Time.time > timeToFire) {
+                timeToFire = Time.time + 1 / fireRate;
+                makeProjectile(direction);
+            }
+        }
+
+    }
+
+    void makeProjectile(Vector2 direction) {
+        GameObject shot = Instantiate(projectile, firePoint.transform.position, Quaternion.identity) as GameObject;
+        Rigidbody2D shot_rg = shot.GetComponent<Rigidbody2D>();
+        shot_rg.velocity = direction * projectileSpeed;
+    }
+}
---
+++ MarioRipoff/Assets/victorscripts/ShootScripts/playerShoot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerShoot : MonoBehaviour {
+
+//    This variable should be on projectile prefab
+//    public LayerMask toHit;
+    public GameObject weapon;
+    IShootable shootable;
+    testControllerScript playermove;
+
+    void Start () {
+        shootable = weapon.GetComponent<IShootable>();
+        playermove = gameObject.GetComponent<testControllerScript>();
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (shootable == null)
+            Debug.Log("No weapon");
+        else {
+            Debug.Log("Has weapon");
+            if (Input.GetButton("Fire1") && Input.GetAxis("Vertical") > 0) {
+                shootable.Shoot(Vector2.up);
+            }
+            //Need to check that the player is grounded
+            else if(Input.GetButton("Fire1") && Input.GetAxis("Vertical") < 0 && !playermove.grounded) {
+                shootable.Shoot(Vector2.down);
+            }
+            //Still need to implement shooting in direction player is facing
+            else if (Input.GetButton("Fire1") && playermove.facingRight) {
+                shootable.Shoot(Vector2.right);
+            }
+            else if (Input.GetButton("Fire1") && !playermove.facingRight) {
+                shootable.Shoot(Vector2.left);
+            }
+                
+        }
+	    
+	}
+
+}
