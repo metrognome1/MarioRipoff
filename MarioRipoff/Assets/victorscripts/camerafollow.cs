e1a6ee48f8c5a7c362ae725635283b9008c68d0f
metrognome1 <onceadayeveryday@gmail.com>
2016-11-13 23:12:12 -0800

    Updated the camera
    Updated the camera so that it properly tracks player even when moving at
    high speeds.

--- MarioRipoff/Assets/victorscripts/camerafollow.cs
+++ MarioRipoff/Assets/victorscripts/camerafollow.cs
@@ -3,49 +3,46 @@ using System.Collections;
 
 public class camerafollow : MonoBehaviour {
 
-    private Transform player;
-    private Vector3 moveTemp;
-
     public GameObject focus;
-
-    public float speed = 3;
     public float xDifference;
     public float yDifference;
-
     public float movementThreshold = 2;
+
+    private Transform player;
+    private Vector3 moveTemp;
+    private Rigidbody2D playerRg2D;
+
 
     void Start ()
     {
         //Change the Square to our player object. Whatever it's string name is.
         player = focus.transform;
         movementThreshold = 2;
+        playerRg2D = focus.GetComponent<Rigidbody2D>();
+    }
+    void Update () {
+        moveCamera();
     }
 
-	void Update () {
-        if (player.transform.position.x > transform.position.x)
-        {
+    private void moveCamera() {
+        if (player.transform.position.x > transform.position.x) {
             xDifference = player.transform.position.x - transform.position.x;
-        } else
-        {
+        }
+        else {
             xDifference = transform.position.x - player.transform.position.x;
         }
 
-        if (player.transform.position.y > transform.position.y)
-        {
+        if (player.transform.position.y > transform.position.y) {
             yDifference = player.transform.position.y - transform.position.y;
-        } else
-        {
+        }
+        else {
             yDifference = transform.position.y - player.transform.position.y;
         }
 
-        // The rate at which the camera moves here should depend on the speed of the focus.
-        // The focus's speed can be calculated using vectors and their speed?
-        if (xDifference >= movementThreshold || yDifference >= movementThreshold)
-        {
+        if (xDifference >= movementThreshold || yDifference >= movementThreshold) {
             moveTemp = player.transform.position;
             moveTemp.z = -1;
-            transform.position = Vector3.MoveTowards(transform.position, moveTemp, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, moveTemp, playerRg2D.velocity.magnitude * Time.deltaTime);
         }
-	
-	}
+    }
 }
